7ab5a34eb996f1da15d43c9ddc7f47838228cadd
Moisis <moisis.george@yahoo.com>
2022-08-03 09:36:26 +0200

    ADDING comments explaining the code and the Task#1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 
 
 fun main() {
+/*
+Task #1
+Initialize  a kotlin project that
+adds 2 Variables together and then prints that summation
+In addition to committing and pushing to  your GitHub Repo
+ */
 
+  //VARIABLES  Declaration
 val firstNumber = 12
 val secondNumber = 4
 
+  //Printing the Summation
   print("The Summation of the First Number and Second Number is " + (firstNumber+secondNumber))
 }
