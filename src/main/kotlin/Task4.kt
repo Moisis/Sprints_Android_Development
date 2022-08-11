1802415b4e68c79d02e9064b112b1f61c74b6736
Moisis <moisis.george@yahoo.com>
2022-08-11 07:27:49 +0200

    Added Task#4

--- src/main/kotlin/Task4.kt
+++ src/main/kotlin/Task4.kt
@@ -1,8 +1,39 @@
+import java.util.Scanner
 
-
-
-
+/*
+* Task #4
+* Using lists and loops write  a program that takes an integer N as an input from user ,
+* then create a List of Strings with Size N
+* That list is filled with the user
+* for each item in the list if it contains Characters A or M (ignore case )
+* print the String in uppercase
+* */
 
 fun main() {
+    val scanner = Scanner(System.`in`)
+    val list2 = mutableSetOf<String>()
+   print("Enter the number of Strings :  ")
+   var N = scanner.nextInt()
 
+    while (N > 0){
+        print("Enter Name : ")
+        val input = scanner.next()
+        list2.add(input)
+        N--
+
+    }
+    for (i in 0 until list2.size){
+        if(check(list2.elementAt(i).toUpperCase())){
+            println(list2.elementAt(i).toUpperCase())
+        }
+    }
 }
+fun check (x : String ) : Boolean {
+  for(i in x.indices){
+     if (x[i]== 'A' || x[i]==  'M' ){
+         return true
+     }
+  }
+return false
+}
+
